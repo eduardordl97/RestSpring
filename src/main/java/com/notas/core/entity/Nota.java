fd9267ff12a82f749d63f06f3e1fae8b7ddfe7e1
eduardordl97 <erdl5259@gmail.com>
2021-08-02 23:15:59 -0500

    Se añade Repositorio y Convertidor

--- src/main/java/com/notas/core/entity/Nota.java
+++ src/main/java/com/notas/core/entity/Nota.java
@@ -1,5 +1,7 @@
 package com.notas.core.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,7 @@ import javax.persistence.Table;
 
 @Table(name = "Nota")
 @Entity
-public class Nota {
+public class Nota implements Serializable{
 	
 	public Nota() {
 		
